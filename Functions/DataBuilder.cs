8b1edc7a51571c3359e11795f28850afaf079cd7
haggau15 <haggau94@gmail.com>
2025-08-18 16:09:13 +0200

    (DataBuilder) added basic function logic

--- Functions/DataBuilder.cs
+++ Functions/DataBuilder.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,18 @@ namespace Greasyfood_datapipeline.Functions
 {
     internal class DataBuilder
     {
+        private readonly ILogger _logger;
+
+        public DataBuilder(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<DataBuilder>();
+        }
+
+        [Function("DataBuilder")]
+        public string Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation("DataBuilder triggered");
+            return "Hello from DataBuilder!";
+        }
     }
 }
