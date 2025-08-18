67edaafb95d4fffc141b400f0699e8aff68af0ce
haggau15 <haggau94@gmail.com>
2025-08-18 16:08:48 +0200

    (ConsumeAPI) added basic function logic

--- Functions/ConsumeAPI.cs
+++ Functions/ConsumeAPI.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Greasyfood_datapipeline.Functions
 {
     internal class ConsumeAPI
     {
+        private readonly ILogger _logger;
+
+        public ConsumeAPI(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ConsumeAPI>();
+        }
+
+        [Function("ConsumeAPI")]
+        public string Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation("ConsumeAPI triggered");
+            return "Hello from ConsumeAPI!";
+        }
     }
 }
