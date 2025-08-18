b76ee89aab339670672fdb7b96414258d0d5463c
haggau15 <haggau94@gmail.com>
2025-08-18 16:09:27 +0200

    (SentimentAnalyze) added basic function logic

--- Functions/SentimentAnalyze.cs
+++ Functions/SentimentAnalyze.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,18 @@ namespace Greasyfood_datapipeline.Functions
 {
     internal class SentimentAnalyze
     {
+        private readonly ILogger _logger;
+
+        public SentimentAnalyze(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SentimentAnalyze>();
+        }
+
+        [Function("SentimentAnalyze")]
+        public string Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation("SentimentAnalyze triggered");
+            return "Hello from SentimentAnalyze!";
+        }
     }
 }
